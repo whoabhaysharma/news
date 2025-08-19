b65368440a156b3c207e33ba8eb1a8e3086b5766
Abhay Sharma <abhay@techcrumb.com>
2025-08-20 01:58:12 +0530

    feat: implement health check endpoint and schedule health check task

--- src/server.js
+++ src/server.js
@@ -3,10 +3,13 @@ import { publishNews } from "./services/newsPublisher.js";
 import logger from "./utils/logger.js";
 import cron from "node-cron";
 import dotenv from "dotenv";
+import fetch from "node-fetch";
 
 dotenv.config();
 
 const CRON_INTERVAL = process.env.CRON_INTERVAL;
+const PORT = process.env.PORT || 3000;
+const SERVER_URL = process.env.SERVER_URL || `http://localhost:${PORT}`;
 
 if (!CRON_INTERVAL) {
   logger.error("CRON_INTERVAL is not defined in the environment variables.");
@@ -24,8 +27,16 @@ cron.schedule(CRON_INTERVAL, async () => {
   }
 });
 
+cron.schedule("*/5 * * * *", async () => {
+  try {
+    logger.info("Hitting empty endpoint for health check.");
+    await fetch(`${SERVER_URL}/health`);
+  } catch (error) {
+    logger.error({ err: error }, "Error hitting health check endpoint.");
+  }
+});
+
 const app = express();
-const PORT = 3000;
 
 app.get("/publish", async (req, res) => {
   try {
@@ -36,6 +47,10 @@ app.get("/publish", async (req, res) => {
     logger.error({ err: error }, "Error publishing news.");
     res.status(500).send("Failed to publish news.");
   }
+});
+
+app.get("/health", (req, res) => {
+  res.status(200).send("Server is healthy.");
 });
 
 app.listen(PORT, () => {
